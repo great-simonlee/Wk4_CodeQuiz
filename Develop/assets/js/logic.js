3ba5b5d443d93b2be6ed1174ee3e4958461526b0
Simon Lee <great.simonlee@gmail.com>
2020-08-24 20:01:46 -0400

    Timer and Question page created

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -18,86 +18,73 @@ var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
 function startQuiz() {
   // hide start screen
- 
+  document.getElementById("start-screen").classList = "hide";
   // un-hide questions section
-
+  questionsEl.classList.remove("hide");
   // start timer
-
   // show starting time
+  clockTick();
 
   getQuestion();
 }
 
 function getQuestion() {
   // get current question object from array
-
   // update title with current question
-
   // clear out any old question choices
-
   // loop over choices
-
-    // create new button for each choice
-
-    // attach click event listener to each choice
-
-    // display on the page
+  // create new button for each choice
+  // attach click event listener to each choice
+  // display on the page
 }
 
 function questionClick() {
   // check if user guessed wrong
-    // penalize time
-
-    // display new time on page
-
-    // play "wrong" sound effect
-
-  // else 
-    // play "right" sound effect
-
-
+  // penalize time
+  // display new time on page
+  // play "wrong" sound effect
+  // else
+  // play "right" sound effect
   // flash right/wrong feedback on page for half a second
-
   // move to next question
-
   // check if we've run out of questions
-    // quizEnd
-  // else 
-    // getQuestion
+  // quizEnd
+  // else
+  // getQuestion
 }
 
 function quizEnd() {
   // stop timer
-
   // show end screen
-
   // show final score
-
   // hide questions section
 }
 
 function clockTick() {
   // update time
+  var timerInterval = setInterval(function () {
+    time--;
+    timerEl.textContent = time;
 
+    if (time === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 20);
   // check if user ran out of time
 }
 
 function saveHighscore() {
   // get value of input box
-
   // make sure value wasn't empty
-    // get saved scores from localstorage, or if not any, set to empty array
-
-    // format new score object for current user
-
-    // save to localstorage
-
-    // redirect to next page
+  // get saved scores from localstorage, or if not any, set to empty array
+  // format new score object for current user
+  // save to localstorage
+  // redirect to next page
 }
 
 function checkForEnter(event) {
   // check if event key is enter
-    // saveHighscore
+  // saveHighscore
 }
 
 // user clicks button to submit initials
